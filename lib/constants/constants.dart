635f5efd558520f8cdb5073636b19796180701f3
Kab-yeon Lee <kabyeon@gmail.com>
2022-02-08 18:54:54 +0900

    Update constants.dart
    xxl support

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,12 +1,13 @@
-List<String> prefixesList = ['xl', 'lg', 'md', 'sm', 'xs'];
+List<String> prefixesList = ['xxl', 'xl', 'lg', 'md', 'sm', 'xs'];
 List<String> prefixesReversedList = prefixesList.reversed.toList();
-List<String> allPrefixList = ['xl', 'lg', 'md', 'sm', 'xs'];
+List<String> allPrefixList = ['xxl', 'xl', 'lg', 'md', 'sm', 'xs'];
 
 double oneColumnRatio = 0.083333;
 double gutterSize = 48.0;
 int numberOfColumns = 12;
 
 mixin Prefixes {
+  static const String xxl = 'xxl';
   static const String xl = 'xl';
   static const String lg = 'lg';
   static const String md = 'md';
